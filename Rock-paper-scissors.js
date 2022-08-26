2f7bdf102d25a61bef23d771462bb2257a62d2b7
Caroline Richert <caroline.richert92@gmail.com>
2022-08-26 14:56:18 +0200

    Adding glow in win lose and draw also cleaned code

--- Rock-paper-scissors.js
+++ Rock-paper-scissors.js
@@ -4,7 +4,9 @@ let compScore = 0;
 const playerScore_span = document.getElementById("player-score");
 const compScore_span = document.getElementById("comp-score");
 const scoreBoard_div = document.querySelector("#scoreboard");
-const result_p = document.querySelector(".result > p");
+const result_p = document.querySelector(".result");
+let playerItem_span = document.querySelector(".playerItem");
+let compItem_span = document.querySelector(".compItem");
 
 //Variables for buttons
 const btn_rock = document.getElementById("rock");
@@ -12,67 +14,77 @@ const btn_paper = document.getElementById("paper");
 const btn_scissors = document.getElementById("scissors");
 
 
-function getComputerChoice () {
+function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"]
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
 
-function win() {
+function items(playerSelection, computerSelection) {
+    playerItem_span.innerHTML = playerSelection;
+    compItem_span.innerHTML = computerSelection;
+}
+
+function win(playerSelection, computerSelection) {
     playerScore++;
     playerScore_span.innerHTML = playerScore;
     compScore_span.innerHTML = compScore;
-    result_div.innerHTML = "Yay! You've won!"
+    items(playerSelection, computerSelection);
+    result_p.innerHTML = `Yay! You've won!`;
+    document.getElementById(playerSelection).classList.add('green-glow');
+    setTimeout(() => document.getElementById(playerSelection).classList.remove('green-glow'), 300);
 }
 
-function lose() {
+function lose(playerSelection, computerSelection) {
     compScore++;
     playerScore_span.innerHTML = playerScore;
     compScore_span.innerHTML = compScore;
-    result_div.innerHTML = "Sorry Bro. Try again!"
+    items(playerSelection, computerSelection);
+    result_p.innerHTML = "Sorry Bro. Try again!";
+    document.getElementById(playerSelection).classList.add('red-glow');
+    setTimeout(() => document.getElementById(playerSelection).classList.remove('red-glow'), 300);
+
 }
 
-function draw() {
+function draw(playerSelection, computerSelection) {
     playerScore_span.innerHTML = playerScore;
     compScore_span.innerHTML = compScore;
-    result_p.innerHTML = "DRAW!"
+    items(playerSelection, computerSelection);
+    result_p.innerHTML = "DRAW!";
+    document.getElementById(playerSelection).classList.add('gray-glow');
+    setTimeout(() => document.getElementById(playerSelection).classList.remove('gray-glow'), 300);
+
 }
 
 function game (playerSelection) {
-    const computerSelection = getComputerChoice();
+    computerSelection = getComputerChoice();
     switch(playerSelection + computerSelection) {
         case "rockscissors":
         case "scissorspaper":
         case "paperrock":
-            win();
+            win(playerSelection, computerSelection);
             break;
         case "rockpaper":
         case "paperscissors":
         case "scissorsrock":
-            lose();
+            lose(playerSelection, computerSelection);
             break;
         case "rockrock":
         case "paperpaper":
         case "scissorsscissors":
-            draw();
+            draw(playerSelection, computerSelection);
             break;
     }
 }
 
-
-
-
-
 function main () {
-    btn_rock.addEventListener('click', function() {
-        game("rock");
-    })
-    btn_paper.addEventListener('click', function() {
-        game("paper");
-    })
-    btn_scissors.addEventListener('click', function() {
-        game("scissors");
-    })
+    btn_rock.addEventListener('click', () => game("rock"));
+    btn_paper.addEventListener('click', () => game("paper"));
+    btn_scissors.addEventListener('click', () => game("scissors"));
 }
+
+//for (let i = 0; i < 5; i++) {
+    // Hier weitermachen.... 5 Rounds -> Then display win, lose or tie!
+ //}
 
 main();
